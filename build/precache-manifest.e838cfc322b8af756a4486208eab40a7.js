cc5510724af5671654f0a808fc8b55213fce5e24
derekdyer0309 <derekdyer02@gmail.com>
2020-06-25 21:30:40 -0700

    ran build

---
+++ build/precache-manifest.e838cfc322b8af756a4486208eab40a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1141ebee566032ce56966cd0ab36da2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e6aff14271b861c1f3",
+    "url": "/static/css/main.36bc1f10.chunk.css"
+  },
+  {
+    "revision": "dc6af5621e95edd38e3e",
+    "url": "/static/js/2.2db9014b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2db9014b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e6aff14271b861c1f3",
+    "url": "/static/js/main.0c0a8ae4.chunk.js"
+  },
+  {
+    "revision": "cb6bf74422421c02dc4f",
+    "url": "/static/js/runtime-main.02afef44.js"
+  }
+]);
